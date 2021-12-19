2cd51114be2af2fe58f1423057fd9810f558f73c
sarathpaul <sarathpaul23@gmail.com>
2021-12-20 01:11:25 +0530

    add datas

--- pages/index.vue
+++ pages/index.vue
@@ -11,6 +11,18 @@ export default {
   name: 'IndexPage',
   components: {
     product
+  },
+  data () {
+    return {
+      products: [
+      { id: 1, name: 'Apple iphone', price: 100000, category: 'smartPhone', img: '', color: '' },
+      { id: 2, name: 'Android TV', price: 25000, category: 'tv', img: '', color: '' },
+      { id: 3, name: 'Macbook pro', price: 190000, category: 'laptop', img: '', color: '' },
+      { id: 4, name: 'Smart watches', price: 5000, category: 'watches', img: '', color: '' },
+      { id: 5, name: 'Shirts', price: 2000, category: 'cloths', img: '', color: '' },
+      { id: 6, name: 'shoes', price: 4000, category: 'footware', img: '', color: '' }
+      ]
+    }
   }
 }
 </script>
